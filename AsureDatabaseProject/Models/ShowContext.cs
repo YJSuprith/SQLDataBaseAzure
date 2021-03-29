584fee7eb357292283d82fb9abc3f832ff8cc448
YJSuprith <yjsuprith02@gmail.com>
2021-03-29 10:58:42 +0530

    commit1

---
+++ AsureDatabaseProject/Models/ShowContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsureDatabaseProject.Models
+{
+    public class ShowContext : DbContext
+    {
+        public ShowContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            {
+                Id = 101,
+                Name = "X-man",
+                Description = "Sci-Fi",
+                Duration = 122.2f
+            });
+        }
+    }
+}
